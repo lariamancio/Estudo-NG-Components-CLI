f2701be649247f321c89a4e171866939db022f94
amanciolari <alveslariamancio@gmail.com>
2024-10-29 12:15:37 -0300

    Configurando o arquivo de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router'
+
 import { FormularioContatoComponent } from "./paginas/formulario-contato/formulario-contato.component";
-import { ListaContatosComponent } from "./paginas/lista-contatos/lista-contatos.component";
+import { ListaContatosComponent } from "./paginas/lista-contatos/lista-contatos.component";;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormularioContatoComponent, ListaContatosComponent],
+  imports: [CommonModule, FormularioContatoComponent, ListaContatosComponent, RouterOutlet],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
